0c873687b073f278712a3e5eb6217bf5fae2d573
EgrSid <e.sidorov2206@gmail.com>
2024-11-07 22:21:58 +0300

    changed the way to calculate loss

--- training.py
+++ training.py
@@ -4,134 +4,159 @@ import time
 import matplotlib.pyplot as plt
 
 
-def train_model(model, criterion, optimizer, epochs, train_loader, test_loader, patience=5, device='cpu'):
-    # Переменные для сохранения метрик
-    train_losses = []
-    valid_losses = []
-    train_accuracies = []
-    valid_accuracies = []
+class Trainer:
+    def __init__(self, model, criterion, optimizer, train_loader, test_loader, epochs=100, patience=5, scheduler=None,
+                 device='cuda'):
+        # Приватные атрибуты
+        self._model = model
+        self._criterion = criterion
+        self._optimizer = optimizer
+        self._train_loader = train_loader
+        self._test_loader = test_loader
+        self._epochs = epochs
+        self._scheduler = scheduler
+        self._patience = patience
+        self._device = device
 
-    # Переменная для ранней остановки
-    best_loss = float('inf')
-    epochs_without_improvement = 0
+        # История метрик
+        self._history = {'train_losses': [], 'valid_losses': [], 'train_accuracies': [], 'valid_accuracies': []}
+        self._best_loss = float('inf')
+        self._best_acc = 0
 
-    model.to(device)
+    # Свойства для доступа к выбранным атрибутам
+    @property
+    def history(self):
+        return self._history
 
-    for epoch in range(epochs):
-        start_time = time.time()
+    @property
+    def model(self):
+        return self._model
 
-        model.train()  # Устанавливаем модель в режим тренировки
-        running_loss = 0.0
-        correct = 0
-        total = 0
+    @property
+    def best_loss(self):
+        return self._best_loss
 
-        # Обучение
-        pbar = tqdm(train_loader, desc=f'Training Epoch {epoch + 1}/{epochs}', leave=False)
-        for inputs, labels in pbar:
-            inputs, labels = inputs.to(device), labels.to(device)
+    @property
+    def best_acc(self):
+        return self._best_acc
 
-            optimizer.zero_grad()  # Обнуляем градиенты
-            outputs = model(inputs)  # Прямой проход
-            loss = criterion(outputs, labels)  # Вычисляем потери
-            loss.backward()  # Обратное распространение
-            optimizer.step()  # Обновляем параметры
+    @property
+    def epochs(self):
+        return self._epochs
 
-            # Статистика
-            running_loss += loss.item()
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+    def train_model(self):
+        self._model.to(self._device)
+        epochs_without_improvement = 0
 
-            # Вывод метрик с помощью tqdm
-            current_loss = running_loss / (len(train_loader) + 1e-10)
-            current_accuracy = correct / (total + 1e-10)
-            pbar.set_description(f'Training Epoch {epoch + 1}/{epochs} - '
-                                 f'Loss: {current_loss:.4f}, Accuracy: {current_accuracy:.4f}')
+        for epoch in range(self._epochs):
+            start_time = time.time()
 
-        # Расчет метрик для обучающего набора
-        train_loss = running_loss / len(train_loader)
-        train_accuracy = correct / total
-        train_losses.append(train_loss)
-        train_accuracies.append(train_accuracy)
+            # Training phase
+            self._model.train()
+            running_loss, correct, total, num_batches = 0.0, 0, 0, 0
 
-        # Валидация
-        model.eval()
-        running_loss = 0.0
-        correct = 0
-        total = 0
-
-        with torch.no_grad():
-            pbar = tqdm(test_loader, desc='Validating', leave=False)
+            pbar = tqdm(self._train_loader, desc=f'Training Epoch {epoch + 1}/{self._epochs}', leave=False)
             for inputs, labels in pbar:
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
+                num_batches += 1
+                inputs, labels = inputs.to(self._device), labels.to(self._device)
+
+                self._optimizer.zero_grad()
+                outputs = self._model(inputs)
+                loss = self._criterion(outputs, labels)
+                loss.backward()
+                self._optimizer.step()
 
                 running_loss += loss.item()
                 _, predicted = torch.max(outputs, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-        # Расчет метрик для валидационного набора
-        valid_loss = running_loss / len(test_loader)
-        valid_accuracy = correct / total
-        valid_losses.append(valid_loss)
-        valid_accuracies.append(valid_accuracy)
+                # Обновляем текущий loss в tqdm
+                current_loss = running_loss / num_batches
+                current_accuracy = correct / total
+                pbar.set_description(f'Training Epoch {epoch + 1}/{self._epochs} - '
+                                     f'Loss: {current_loss:.4f}, Accuracy: {current_accuracy:.4f}')
 
-        # Раннее прекращение обучения
-        if valid_loss < best_loss:
-            best_loss = valid_loss
-            epochs_without_improvement = 0
-        else:
-            epochs_without_improvement += 1
+            train_loss = running_loss / num_batches
+            train_accuracy = correct / total
+            self._history['train_losses'].append(train_loss)
+            self._history['train_accuracies'].append(train_accuracy)
 
-        if epochs_without_improvement >= patience:
-            print(f'\nEarly stopping triggered after {epoch + 1} epochs')
-            break
+            # Validation phase
+            self._model.eval()
+            running_loss, correct, total, num_batches = 0.0, 0, 0, 0
 
-        end_time = time.time()
-        epoch_time = end_time - start_time
-        print(f'Epoch {epoch + 1}/{epochs} - '
-              f'Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, '
-              f'Valid Loss: {valid_loss:.4f}, Valid Accuracy: {valid_accuracy:.4f} - '
-              f'Time: {epoch_time:.2f}s')
+            with torch.no_grad():
+                pbar = tqdm(self._test_loader, desc='Validating', leave=False)
+                for inputs, labels in pbar:
+                    num_batches += 1
+                    inputs, labels = inputs.to(self._device), labels.to(self._device)
+                    outputs = self._model(inputs)
+                    loss = self._criterion(outputs, labels)
 
-    return model, {
-        'train_losses': train_losses,
-        'valid_losses': valid_losses,
-        'train_accuracies': train_accuracies,
-        'valid_accuracies': valid_accuracies
-    }
+                    running_loss += loss.item()
+                    _, predicted = torch.max(outputs, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+            valid_loss = running_loss / num_batches
+            valid_accuracy = correct / total
+            self._history['valid_losses'].append(valid_loss)
+            self._history['valid_accuracies'].append(valid_accuracy)
+
+            # Early stopping
+            if valid_loss < self._best_loss:
+                self._best_loss = valid_loss
+                self._best_acc = valid_accuracy
+                epochs_without_improvement = 0
+            else:
+                epochs_without_improvement += 1
+
+            if epochs_without_improvement >= self._patience:
+                print(f'\nEarly stopping triggered after {epoch} epochs')
+                break
+
+            # Шаг scheduler
+            if self._scheduler:
+                try:
+                    self._scheduler.step()
+                except Exception as e:
+                    print(f'Error running scheduler: {e}')
+
+            end_time = time.time()
+            epoch_time = end_time - start_time
+            print(f'Epoch {epoch + 1}/{self._epochs} - '
+                f'Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, '
+                f'Valid Loss: {valid_loss:.4f}, Valid Accuracy: {valid_accuracy:.4f} - '
+                f'Time: {epoch_time:.2f}s')
 
 
-def plot_metrics(metrics):
-    # Извлечение данных
-    train_losses = metrics['train_losses']
-    valid_losses = metrics['valid_losses']
-    train_accuracies = metrics['train_accuracies']
-    valid_accuracies = metrics['valid_accuracies']
 
-    epochs = range(1, len(train_losses) + 1)
+    def plot_metrics(self):
+        train_losses = self._history['train_losses']
+        valid_losses = self._history['valid_losses']
+        train_accuracies = self._history['train_accuracies']
+        valid_accuracies = self._history['valid_accuracies']
 
-    # plot для потерь
-    plt.figure(figsize=(12, 5))
+        epochs = range(1, len(train_losses) + 1)
 
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs, train_losses, label='Train Loss', color='blue')
-    plt.plot(epochs, valid_losses, label='Valid Loss', color='orange')
-    plt.title('Train and Validation Loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
+        plt.figure(figsize=(12, 5))
 
-    # pllot для точности
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs, train_accuracies, label='Train Accuracy', color='blue')
-    plt.plot(epochs, valid_accuracies, label='Valid Accuracy', color='orange')
-    plt.title('Train and Validation Accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs, train_losses, label='Train Loss', color='blue')
+        plt.plot(epochs, valid_losses, label='Valid Loss', color='orange')
+        plt.title('Train and Validation Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
 
-    plt.tight_layout()
-    plt.show()
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, train_accuracies, label='Train Accuracy', color='blue')
+        plt.plot(epochs, valid_accuracies, label='Valid Accuracy', color='orange')
+        plt.title('Train and Validation Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+
+        plt.tight_layout()
+        plt.show()
